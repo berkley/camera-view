4c2505d197d4a77dfe2a67e13dee57c9fe2205a5
Chad Berkley <berkley@Slickrock.local>
2009-12-09 21:04:11 -0800

    adding protocol

---
+++ CameraViewProtocol.h
@@ -0,0 +1,15 @@
+//
+//  CameraViewProtocol.h
+//  camera-view
+//
+//  Created by Chad Berkley on 12/9/09.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@protocol CameraViewProtocol
+
+- (void)didFinishPickingVideo:(NSURL *)url;
+
+@end
